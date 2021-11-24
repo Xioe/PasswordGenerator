86d75c00aecd469aaa4cb1a003ad64baa880af05
Mateusz Burnagiel <mateuszburnagiel@gmail.com>
2021-11-24 20:51:02 +0100

    View is almost ready, now prepare to JS

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+const numbers = document.querySelector('#number');
+const uppercase = document.querySelector('#uppercase');
+const lowercase = document.querySelector('#lowercase');
+const symbols = document.querySelector('#symbols');
+const lenght = document.querySelector('#lenght')
+
+const generateCustomPass = () => {
+    
+}
